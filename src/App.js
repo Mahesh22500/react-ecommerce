46aeeec76c8a1cfb1c2fa3776ad9ce09a1851ae4
Mahesh22500 <maheshsingh22500@gmail.com>
2024-08-18 02:00:31 +0530

    added some react pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
+
+
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home></Home>),
+  },
+  {
+    path: "/signup",
+    element: (<SignupPage></SignupPage>),
+  },
+  {
+    path: "/login",
+    element: (<LoginPage></LoginPage>),
+  },
+  {
+    path:"/cart",
+    element:(<CartPage></CartPage>)
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+<RouterProvider router={router} />
+
+
+    </>
+    
   );
 }
 
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login  from "../features/auth/components/Login.jsx"
+
+
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  )
+}
+
+export default LoginPage
+
